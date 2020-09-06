246e0b3abf974a488a32388aa9b35415ce8e4dc8
xrudoga <defcw58@gmail.com>
2020-09-06 18:14:35 +0800

    Adding CORS support to the API

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,14 @@ namespace API
                 opt.UseSqlite(Configuration.GetConnectionString
                 ("DefaultConnection"));
             });
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
+            
             services.AddControllers();
         }
 
@@ -46,8 +54,10 @@ namespace API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
+            
 
             app.UseEndpoints(endpoints =>
             {
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,17 +1,32 @@
 import React, {Component} from 'react';
 import './App.css';
-import { cars } from './demo';
-import CarItem from './CarItem';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    values: []
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/values')
+      .then((response) => {
+        console.log(response)
+        this.setState({
+          values: response.data
+        })
+      })
+  }
+
   render() {
     return (
       <div className="App">
-        <ul>
-        {cars.map((car) => (
-          <CarItem car={car} />
-        ))}
-        </ul>
+        <header className='App-header'>
+          <ul>
+            {this.state.values.map((value: any) => (
+              <li>{value.name}</li>
+            ))}
+          </ul>
+        </header>
       </div>
     );
   }
