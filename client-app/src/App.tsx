c2e48a5dec5d526c71fba52e56ac4e6a11ef6674
xrudoga <defcw58@gmail.com>
2020-09-08 14:32:45 +0800

    Creating our first Query handler

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { Header, Icon, List } from 'semantic-ui-react'
 import './App.css';
 import axios from 'axios';
 
@@ -10,7 +11,6 @@ class App extends Component {
   componentDidMount() {
     axios.get('http://localhost:5000/api/values')
       .then((response) => {
-        console.log(response)
         this.setState({
           values: response.data
         })
@@ -19,14 +19,16 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className='App-header'>
-          <ul>
-            {this.state.values.map((value: any) => (
-              <li>{value.name}</li>
-            ))}
-          </ul>
-        </header>
+      <div>
+        <Header as='h2'>
+          <Icon name='users' />
+          <Header.Content>Reactivities</Header.Content>
+        </Header>
+        <List>
+          {this.state.values.map((value: any) => (
+            <List.Item key={value.id}>{value.name}</List.Item>
+          ))}
+        </List>
       </div>
     );
   }
