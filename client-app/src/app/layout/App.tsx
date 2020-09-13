90097ca412e253fd58fa0ccd0a2c7e699cfc5700
xrudoga <defcw58@gmail.com>
2020-09-13 20:42:00 +0800

    Building a CRUD application in React

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -10,36 +10,61 @@ const App = () => {
 
   const [activities, setActivities] = useState<IActivity[]>([])
   const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null)
+  const [editMode, setEditMode] = useState(false)
 
   const handleSelectActivity = (id: string) => {
     setSelectedActivity(activities.filter(a => a.id === id)[0])
+    setEditMode(false)
+  }
+
+  const handleOpenCreateForm = () => {
+    setSelectedActivity(null)
+    setEditMode(true)
+  }
+
+  const handleCreateActivity = (activity: IActivity) => {
+    setActivities([...activities, activity])
+    setSelectedActivity(activity)
+    setEditMode(false)
+  }
+
+  const handleEditActivity = (activity: IActivity) => {
+    setActivities([...activities.filter(a => a.id !== activity.id), activity])
+    setSelectedActivity(activity)
+    setEditMode(false)
+  }
+
+  const handleDeleteActivity = (id: string) => {
+    setActivities([...activities.filter(a => a.id !== id)])
   }
 
   useEffect(() => {
     axios
       .get<IActivity[]>('http://localhost:5000/api/activities')
       .then((response) => {
+        let activities = []
+        response.data.forEach(activity => {
+          activity.date =activity.date.split('.')[0]
+          activities.push(activity)
+        })
         setActivities(response.data)
       })
   }, [])
 
-  // componentDidMount() {
-  //   axios.get<IActivity[]>('http://localhost:5000/api/activities')
-  //     .then((response) => {
-  //       this.setState({
-  //         activities: response.data
-  //       })
-  //     })
-  // }
-
   return (
     <>
-      <NavBar />
+      <NavBar OpenCreateForm={handleOpenCreateForm} />
       <Container style={{marginTop: '7em'}}>
         <ActivityDashboard 
           activities={activities} 
           selectActivity={handleSelectActivity} 
           selectedActivity={selectedActivity!}
+          editMode={editMode}
+          setEditMode={setEditMode}
+          setSelectedActivity={setSelectedActivity}
+          createActivity={handleCreateActivity}
+          editActivity={handleEditActivity}
+          deleteActivity={handleDeleteActivity}
         />
       </Container>
     </>
