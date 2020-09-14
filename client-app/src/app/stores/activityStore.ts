93bf5ddad593774f7bf50d29bfbca4d325c5e636
xrudoga <defcw58@gmail.com>
2020-09-14 20:32:22 +0800

    Cleaning up our code

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,7 +1,9 @@
-import { observable, action, computed } from 'mobx'
-import { createContext } from 'react'
-import agent from '../../api/agent';
-import { IActivity } from '../models/activity';
+import { observable, action, computed, configure } from 'mobx'
+import { createContext, SyntheticEvent } from 'react'
+import agent from '../../api/agent'
+import { IActivity } from '../models/activity'
+
+configure({enforceActions: 'always'})
 
 class ActivityStore {
     @observable activityRegistry = new Map()
@@ -10,6 +12,7 @@ class ActivityStore {
     @observable loadingInitial = false
     @observable editMode = false
     @observable submitting = false
+    @observable target = ''
 
     @computed get activitiesByDate() {
         return Array.from(this.activityRegistry.values()).sort(
@@ -23,7 +26,6 @@ class ActivityStore {
         try {
             activities.forEach((activity) => {
                 activity.date =activity.date.split('.')[0]
-                // this.activities.push(activity)
                 this.activityRegistry.set(activity.id, activity)
             })
             this.loadingInitial = false
@@ -46,13 +48,53 @@ class ActivityStore {
         }
     }
 
+    @action editActivity = async (activity: IActivity) => {
+        this.submitting = true
+        try {
+            agent.Activities.update(activity)
+            this.activityRegistry.set(activity.id, activity)
+            this.selectedActivity = activity
+            this.editMode = false
+        } catch(error) {
+            this.submitting = false
+            console.log(error)
+        }
+    }
+
+    @action deleteActivity = async (event: SyntheticEvent<HTMLButtonElement>,id: string) => {
+        this.submitting = true
+        this.target = event.currentTarget.name
+        try {
+            await agent.Activities.delete(id)
+            this.activityRegistry.delete(id)
+            this.submitting = false
+            this.target = ''
+        } catch(error) {
+            this.submitting = false
+            this.target = ''
+            console.log(error)
+        }
+    }
+
     @action openCreateForm = () => {
         this.editMode = true
         this.selectedActivity = undefined
     }
 
+    @action openEditForm = (id: string) => {
+        this.selectActivity = this.activityRegistry.get(id)
+        this.editMode = true
+    }
+
+    @action cancelSelectedActivity = () => {
+        this.selectedActivity = undefined
+    }
+
+    @action cancelFormOpen = () => {
+        this.editMode = false
+    }
+
     @action selectActivity = (id: string) => {
-        // this.selectedActivity = this.activities.find(a => a.id === id)
         this.selectedActivity = this.activityRegistry.get(id)
         this.editMode = false
     }
