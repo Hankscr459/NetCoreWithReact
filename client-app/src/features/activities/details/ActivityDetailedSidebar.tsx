9ba64b1309ce7337b62212ab5377dfdf19f00957
xrudoga <defcw58@gmail.com>
2020-09-15 22:28:46 +0800

    Styling the Activity Details page

---
+++ client-app/src/features/activities/details/ActivityDetailedSidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ActivityDetailedSidebar = () => {
+    return (
+        <div>
+            <h1>Siderbar</h1>
+        </div>
+    )
+}
+
+export default ActivityDetailedSidebar
