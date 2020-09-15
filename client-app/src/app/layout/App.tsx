2f8383be3601a663d268012b18de70e17ab7e8b4
xrudoga <defcw58@gmail.com>
2020-09-15 14:15:58 +0800

    Moving the home page outside of our navigation routes

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react'
+import React, { useEffect, useContext, Fragment } from 'react'
 import {  Container } from 'semantic-ui-react'
 import NavBar from '../../features/nav/NavBar'
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard'
@@ -23,13 +23,24 @@ const App: React.FC<RouteComponentProps> = ({location}) => {
 
   return (
     <>
-      <NavBar />
-      <Container style={{marginTop: '7em'}}>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/activities' component={ActivityDashboard} />
-        <Route path='/activities/:id' component={ActivityDetails} />
-        <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
-      </Container>
+      <Route exact path='/' component={HomePage} />
+      <Route
+        path={'/(.+)'}
+        render={() => (
+          <>
+            <NavBar />
+            <Container style={{marginTop: '7em'}}>
+              <Route exact path='/activities' component={ActivityDashboard} />
+              <Route path='/activities/:id' component={ActivityDetails} />
+              <Route 
+                key={location.key} 
+                path={['/createActivity', '/manage/:id']} 
+                component={ActivityForm} 
+              />
+            </Container>
+          </>
+        )}
+      />
     </>
   );
 }
