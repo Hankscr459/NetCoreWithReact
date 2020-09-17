5b6bd8abe3aec9e2e9601f71479dd952e951a0e2
xrudoga <defcw58@gmail.com>
2020-09-17 11:58:31 +0800

    Creating a reusable Select input field

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -8,6 +8,8 @@ import { RouteComponentProps } from 'react-router-dom';
 import { Form as FinalForm, Field } from 'react-final-form'
 import TextInput from '../../../app/common/form/TextInput';
 import TextAreaInput from '../../../app/common/form/TextAreaInput';
+import SelectInput from '../../../app/common/form/SelectInput';
+import { category } from '../../../app/common/options/categoryOptions';
 
 interface DetailParams {
     id: string
@@ -100,9 +102,10 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
                             />
                             <Field
                                 name='category'
+                                options={category}
                                 placeholder='Category' 
                                 value={activity.category} 
-                                component={TextInput}
+                                component={SelectInput}
                             />
                             <Field
                                 name='date' 
