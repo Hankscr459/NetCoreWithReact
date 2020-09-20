c61e7f34a64896a6dd9f55687034bfd96df556af
xrudoga <defcw58@gmail.com>
2020-09-20 14:15:08 +0800

    Hooking up the Login form to the API

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,12 +1,10 @@
-import { observable, action, computed, configure, runInAction } from 'mobx'
-import { createContext, SyntheticEvent } from 'react'
+import { observable, action, computed, runInAction } from 'mobx'
+import { SyntheticEvent } from 'react'
 import agent from '../../api/agent'
 import { IActivity } from '../models/activity'
 import { history } from '../../index'
 import { toast } from 'react-toastify'
 import { RootStore } from './rootStore'
-
-configure({enforceActions: 'always'})
 
 export default class ActivityStore {
     rootStore: RootStore
