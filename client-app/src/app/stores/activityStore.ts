8f5497eb81392591f96bf4f2bfff4d20e779e546
xrudoga <defcw58@gmail.com>
2020-11-10 17:06:03 +0800

    Adding the SignalR hub connection to the client

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -5,7 +5,8 @@ import { IActivity } from '../models/activity'
 import { history } from '../../index'
 import { toast } from 'react-toastify'
 import { RootStore } from './rootStore'
-import { setActivityProps, createAttendee } from '../common/utli/utli';
+import { setActivityProps, createAttendee } from '../common/utli/utli'
+import { HubConnection, HubConnectionBuilder, LogLevel } from '@microsoft/signalr'
 
 export default class ActivityStore {
     rootStore: RootStore
@@ -19,6 +20,28 @@ export default class ActivityStore {
     @observable submitting = false
     @observable target = ''
     @observable loading = false
+    @observable.ref hubConnection: HubConnection | null = null;
+
+    @action createHubConnect = () => {
+        this.hubConnection = new HubConnectionBuilder()
+            .withUrl('http://localhost:5000/chat', {
+                accessTokenFactory: () => this.rootStore.commonStore.token!
+            })
+            .configureLogging(LogLevel.Information)
+            .build()
+        this.hubConnection
+            .start()
+            .then(() => console.log(this.hubConnection!.state))
+            .catch(error => console.log('Error establishing connecting: ', error))
+
+        this.hubConnection.on('ReceiveComment', comment => {
+            this.activity!.comments.push(comment)
+        })
+    }
+
+    @action stopHubConnection = () => {
+        this.hubConnection!.stop()
+    }
 
     @computed get activitiesByDate() {
         return this.groupActivitiesByDate(Array.from(this.activityRegistry.values()))
