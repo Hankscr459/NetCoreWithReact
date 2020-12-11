3006fffc0131e19b4db807f17e845d9d88dbd64f
xrudoga <defcw58@gmail.com>
2020-12-11 18:19:45 +0800

    Adding the Filtering methods in the client

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Interfaces;
 using AutoMapper;
 using Domain;
 using MediatR;
@@ -10,40 +11,68 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Persistence;
 
-namespace Application.Activities {
-    public class List {
-        public class ActivitiesEnvelope {
+namespace Application.Activities
+{
+    public class List
+    {
+        public class ActivitiesEnvelope
+        {
             public List<ActivityDto> Activities { get; set; }
             public int ActivityCount { get; set; }
         }
-        public class Query : IRequest<ActivitiesEnvelope> {
-            public Query (int? limit, int? offset) 
+        public class Query : IRequest<ActivitiesEnvelope>
+        {
+            public Query(int? limit, int? offset, bool isGoing, bool isHost, DateTime? startDate)
             {
-                this.Limit = limit;
-                this.Offset = offset;
+                Limit = limit;
+                Offset = offset;
+                IsGoing = isGoing;
+                IsHost = isHost;
+                StartDate = startDate ?? DateTime.Now;
             }
             public int? Limit { get; set; }
             public int? Offset { get; set; }
+            public bool IsGoing { get; set; }
+            public bool IsHost { get; set; }
+            public DateTime? StartDate { get; set; }
         }
 
-        public class Handler : IRequestHandler<Query, ActivitiesEnvelope> {
+        public class Handler : IRequestHandler<Query, ActivitiesEnvelope>
+        {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
-            public Handler (DataContext context, IMapper mapper) {
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
+            {
+                _userAccessor = userAccessor;
                 _mapper = mapper;
                 _context = context;
             }
 
-            public async Task<ActivitiesEnvelope> Handle (Query request,
-                CancellationToken cancellationToken) 
+            public async Task<ActivitiesEnvelope> Handle(Query request,
+                CancellationToken cancellationToken)
             {
-                var queryable = _context.Activities.AsQueryable();
+                var queryable = _context.Activities
+                    .Where(x => x.Date >= request.StartDate)
+                    .OrderBy(x => x.Date)
+                    .AsQueryable();
+
+                if (request.IsGoing && !request.IsHost)
+                {
+                    queryable = queryable.Where(x => x.UserActivities.Any(a => a.AppUser.UserName == _userAccessor.GetCurrentUsername()));
+                }
+
+                if (request.IsHost && !request.IsGoing)
+                {
+                    queryable = queryable.Where(x => x.UserActivities.Any(a => a.AppUser.UserName == _userAccessor.GetCurrentUsername() && a.IsHost));
+                }
+
                 var activities = await queryable
                     .Skip(request.Offset ?? 0)
                     .Take(request.Limit ?? 3).ToListAsync();
                 return new ActivitiesEnvelope
                 {
-                    Activities = _mapper.Map<List<Activity>, List<ActivityDto>> (activities),
+                    Activities = _mapper.Map<List<Activity>, List<ActivityDto>>(activities),
                     ActivityCount = queryable.Count()
                 };
             }
